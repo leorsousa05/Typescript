517c096be899e534e4af46647815727a7ae7e04d
Leorsousa05 <leorsousa05@gmail.com>
2022-06-21 20:47:32 -0300

    Initial commit

---
+++ introduction/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const me = {
+    name: 'Leonardo',
+    age: 17
+};
+/* No typescript para dar um export, usamos
+a mesma sintaxe de modulos do node. */
+exports.default = me;
---
+++ introduction/index.ts
@@ -0,0 +1,8 @@
+const me = {
+    name: 'Leonardo',
+    age: 17
+}
+
+/* No typescript para dar um export, usamos
+a mesma sintaxe de modulos do node. */
+export default me
